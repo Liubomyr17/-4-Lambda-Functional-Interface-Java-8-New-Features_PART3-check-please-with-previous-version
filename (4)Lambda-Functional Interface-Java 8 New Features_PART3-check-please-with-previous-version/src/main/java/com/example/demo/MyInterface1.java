80d8e4a6457011923bb245dbf6141e1755162642
L&J Brothers <41821541+Liubomyr17@users.noreply.github.com>
2019-12-14 20:33:41 +0200

    Add files via upload

---
+++ (4)Lambda-Functional Interface-Java 8 New Features_PART3-check-please-with-previous-version/src/main/java/com/example/demo/MyInterface1.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+@FunctionalInterface
+public interface MyInterface1 {
+
+    void method1();
+
+}
---
+++ (4)Lambda-Functional Interface-Java 8 New Features_PART3-check-please-with-previous-version/src/main/java/com/example/demo/MyInterface2.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+@FunctionalInterface
+public interface MyInterface2 extends MyInterface1 {
+
+
+}
